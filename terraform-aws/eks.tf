23fc980ec5186760a1f92a115e441ab25bc3a243
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-07-18 12:57:05 +0200

    test

--- terraform-aws/eks.tf
+++ terraform-aws/eks.tf
@@ -10,7 +10,7 @@ module "eks" {
   endpoint_public_access  = true
   endpoint_private_access = true
 
-  enable_cluster_creator_admin_permissions = true
+  enable_cluster_creator_admin_permissions = false
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
